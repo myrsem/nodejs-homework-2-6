206fce8845d964cd9f06d3ba6b7cabcb16114ad7
Myroslav Semeniuk <97482927+myrsem@users.noreply.github.com>
2023-01-13 15:32:53 +0200

    edits in middleware

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -6,23 +6,26 @@ const {HttpError} = require("../helpers")
 
 const {SECRET_KEY} = process.env;
 
-const authenticate = async(req, res, next)=> {
-    const {authorization = ""} = req.headers;
-    const [bearer, token] = authorization.split(" ");
-    if(bearer !== "Bearer") {
-        next(HttpError(401))
-    }
+const authenticate = async(req, res, next) => {
     try {
-        const {id} = jwt.verify(token, SECRET_KEY)
+        const {authorization = ""} = req.headers;
+        const [bearer, token] = authorization.split(" ");
+        if(bearer !== "Bearer" || !token) {
+            throw HttpError(401);
+        }
+        const {id} = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
-        if(!user || !user.token) {
-            next(HttpError(401))
+        if(!user || user.token !== token) {
+            throw HttpError(401);
         }
         req.user = user;
         next()
     }
-    catch {
-        next(HttpError(401))
+    catch(error) {
+        if(!error.status) {
+            error.status = 401;
+        }
+        next(error);
     }
 }
 
